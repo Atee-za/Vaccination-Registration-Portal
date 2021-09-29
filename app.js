aa5c18d72ea7468b1dbb2ba5219802533bef7009
215138848 <215138848@mycput.ac.za>
2021-09-29 17:49:35 +0200

    post method

--- app.js
+++ app.js
@@ -6,15 +6,16 @@ const app = express();
 
 //mongo db link
 const dbURI = 'mongodb+srv://VRPuser:vrp654word@vrp.ivfk4.mongodb.net/vrp-set?retryWrites=true&w=majority';
-mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
-  .then((result) => app.listen(3000))
-  .catch((err) => console.log(err));
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true});
+//  .then((result) => app.listen(3000))
+//  .catch((err) => console.log(err));
 
+/*
 app.get('/add-blog', (req, res) => {
   const blog = new Blog({
-    title: 'testing01',
-    name: 'Basic Test',
-    age: '1'
+    title: 'testing02',
+    name: 'Basic Test number 2',
+    age: '2'
   });
 
   blog.save()
@@ -24,13 +25,41 @@ app.get('/add-blog', (req, res) => {
     .catch((err) => {
       console.log(err);
     });
-}); 
+});
 
-  app.set('view engine', 'ejs');
+app.get('/all-blogs', (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.send(result)
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get('/single-blogs', (req, res) => {
+  Blog.findById('615466a35b22a29a700a4f63')
+    .then((result) => {
+      res.send(result)
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+*/
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true}));
 
 app.get('/', (req, res) => {
   res.render('index');
-}); 
+});
+
+app.post('/blogs', (req, res) => {
+  console.log(req.body);
+})
 
 app.get('/step1', (req, res) => {
   res.render('step1');
