74bece9814d41fede40c6be21a7b7a7d018f4ff3
215138848 <215138848@mycput.ac.za>
2021-10-07 18:04:56 +0200

    pages 0-3 connected, forms done

---
+++ models/personalInfo.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const personalI = new Schema({
+    ID_Number: {type: String, required: true},
+    Passport: {type: String, required: false},
+    Dob: {type: Date, required: true},
+    Name: {type: String, required: true},
+    Surname: {type: String, required: true},
+
+}, {timestamps: true});
+
+const personalInfo = mongoose.model('personalInfo', personalI);
+
+module.exports = personalInfo;
