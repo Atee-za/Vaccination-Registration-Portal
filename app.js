207353a8544521dc74f79fb75c1a23c72e052d6f
Athenkosi <athenkosizono@gmail.com>
2021-10-07 10:50:47 +0200

    Updated App

--- app.js
+++ app.js
@@ -4,18 +4,26 @@ const Blog = require('./models/blog');
 
 const app = express();
 
-//mongo db link
-const dbURI = 'mongodb+srv://VRPuser:vrp654word@vrp.ivfk4.mongodb.net/vrp-set?retryWrites=true&w=majority';
+//connect to database
+var mongoDB = 'mongodb+srv://athi:athi2021@cluster0.ious3.mongodb.net/Vaccination_Registration_Portal?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+//mongo db link 
+DELETE THIS
+/*const dbURI = 'mongodb+srv://athi:athi2021@cluster0.ious3.mongodb.net/Vaccination Registration Portal?retryWrites=true&w=majority';
 mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
   .then((result) => app.listen(3000))
-  .catch((err) => console.log(err));
+  .catch((err) => console.log("Connecting to MongoDB err")); */
 
 app.get('/add-blog', (req, res) => {
   const blog = new Blog({
     title: 'testing01',
     name: 'Basic Test',
     age: '1'
-  });
+  }); 
 
   blog.save()
     .then((result) => {
@@ -25,6 +33,7 @@ app.get('/add-blog', (req, res) => {
       console.log(err);
     });
 }); 
+
 
   app.set('view engine', 'ejs');
 
